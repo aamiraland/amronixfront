86fcdf46e1225d4df3465885680ae650fcf371f8
aamiraland <138506324+aamiraland@users.noreply.github.com>
2024-06-25 21:06:06 +0530

    Update Item.jsx

--- src/Components/Items/Item.jsx
+++ src/Components/Items/Item.jsx
@@ -10,12 +10,13 @@ const Item = (props) => {
           <img onClick={window.scrollTo(0, 0)} src={props.image} alt="" />
        
         <p>{props.name}</p> 
+          </Link>
+          
         <p>{props.category}</p>
         <div className="item-prices">
           <div className="item-price-new">₹{props.new_price}</div>
           <div className="item-price-old">₹{props.old_price}</div>
         </div>
-          </Link>
       </div>
     </div>
   );
